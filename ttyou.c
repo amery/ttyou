c302a21ace7c9dac0c97c126d9e4d5f1c0ccb473
Alejandro Mery <alejandro.mery@micrologic.net>
2010-12-03 10:12:34 +0100

    ttyou: register chrdev region

--- ttyou.c
+++ ttyou.c
@@ -1,18 +1,36 @@
 #include <linux/init.h>
 #include <linux/module.h>
 
-MODULE_LICENSE("Dual BSD/GPL");
+#include <linux/fs.h>
+#include <linux/kdev_t.h>
+
+#define TTYOU_MASTER_NAME	"ttyou"
+#define TTYOU_SLAVE_MAX		7
+
+static dev_t master;
 
 static int ttyou_init(void)
 {
-	printk(KERN_ALERT "Hello world\n");
+	int ret = alloc_chrdev_region(&master, 0, TTYOU_SLAVE_MAX+1,
+			       TTYOU_MASTER_NAME);
+	if (ret < 0) {
+		printk(KERN_ALERT "ttyou: failed to register (%d)\n", ret);
+		return ret;
+	}
+
+	printk(KERN_ALERT "ttyou: registered with major %d\n", MAJOR(master));
 	return 0;
 }
 
 static void ttyou_exit(void)
 {
-	printk(KERN_ALERT "Goodbye cruel world\n");
+	unregister_chrdev_region(master, TTYOU_SLAVE_MAX+1);
+	printk(KERN_ALERT "ttyou: bye\n");
 }
 
 module_init(ttyou_init);
 module_exit(ttyou_exit);
+
+MODULE_LICENSE("Dual BSD/GPL");
+MODULE_AUTHOR("Alejandro Mery <alejandro.mery@micrologic.net>");
+MODULE_DESCRIPTION("TTY on Userspace");
